f27a5c6f3189bad8f7a1a059618b1591897d3a17
Bidisha Mondal <bidisha_mondal@intuit.com>
2021-07-16 00:26:56 +0530

    removed unused files and added constants folder

---
+++ src/constants/commonConstants.ts
@@ -0,0 +1,47 @@
+export const AVATAR_GENDER = {
+  MALE: 'MALE',
+  FEMALE: 'FEMALE'
+};
+
+export const VISUAL_ATTRIBUTES = [
+  {
+    key: 'FACE_SHAPE',
+    gender: 'BOTH'
+  },
+  {
+    key: 'EYE_SHAPE',
+    gender: 'BOTH'
+  },
+  {
+    key: 'EYE_COLOR',
+    gender: 'BOTH'
+  },
+  {
+    key: 'EYEBROW_SHAPE',
+    gender: 'BOTH'
+  },
+  {
+    key: 'NOSE_SHAPE',
+    gender: 'BOTH'
+  },
+  {
+    key: 'MOUTH_SHAPE',
+    gender: 'BOTH'
+  },
+  {
+    key: 'FACIAL_HAIR',
+    gender: 'MALE'
+  },
+  {
+    key: 'HEAD_ACCESSORIES',
+    gender: 'BOTH'
+  },
+  {
+    key: 'FACE_ACCESSORIES',
+    gender: 'BOTH'
+  },
+  {
+    key: 'SUPERPOWER',
+    gender: 'BOTH'
+  }
+];
